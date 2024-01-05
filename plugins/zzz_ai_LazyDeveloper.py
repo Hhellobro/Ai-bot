e9b25646b5d6e7a0b2e958aeee874157bb404010
Hhellobro <154648634+Hhellobro@users.noreply.github.com>
2024-01-05 23:01:44 +0530

    Update zzz_ai_LazyDeveloper.py

--- plugins/zzz_ai_LazyDeveloper.py
+++ plugins/zzz_ai_LazyDeveloper.py
@@ -27,13 +27,13 @@ async def lazy_answer(client, message):
                         [InlineKeyboardButton(text=f"🗑 Delete log ❌", callback_data=f'close_data')],
                     ]
                 reply_markup=InlineKeyboardMarkup(btn)
-                footer_credit = "🦋<a href='https://telegram.me/LazyDeveloperSupport'>• ʀᴇᴘᴏʀᴛ ɪꜱꜱᴜᴇ •</a>══<a href='https://telegram.me/LazyDeveloperr'>• ᴄᴏɴᴛᴀᴄᴛ ᴍᴀꜱᴛᴇʀ •</a>🦋"
+                footer_credit = "🦋<a href='https://telegram.me/AlphaDeveloperssSupport'>• ʀᴇᴘᴏʀᴛ ɪꜱꜱᴜᴇ •</a>══<a href='https://telegram.me/AlphaDeveloperss'>• ᴄᴏɴᴛᴀᴄᴛ ᴍᴀꜱᴛᴇʀ •</a>🦋"
                 lazy_response = response.choices[0].text 
                 await client.send_message(LAZY_AI_LOGS, text=f"⚡️⚡️#Lazy_AI_Query \n\n• A user named **{message.from_user.mention}** with user id - `{user_id}`. Asked me this query...\n\n══❚█══Q   U   E   R   Y══█❚══\n\n\n[Q྿.]**{lazy_users_message}**\n\n👇Here is what i responded:\n:-`{lazy_response}`\n\n\n❚═USER ID═❚═• `{user_id}` \n❚═USER Name═❚═• `{message.from_user.mention}` \n\n🗃️" , reply_markup = reply_markup )
                 await message.reply(f"{lazy_response}\n\n\n{footer_credit}")
             except Exception as error:
                 print(error)
-                await message.reply_text(f'Error aa gya Lazy Bhaiya 😀\n\n{error}')
+                await message.reply_text(f'An error detected boss(AlphaDeveloper)🕵‍♀️\n\n{error}')
     else:
         return
     
@@ -45,47 +45,10 @@ async def lazy_answer(client, message):
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-
-
-
-
-
-
-        # i am NOT INTRESTED IN BEING YOUR SECOND FATHER... SO DON'T REMOVE MY CREDIT...
-        #                               @LazyDeveloperr
-
-
-
-
-
-
-
-
-
-
-
         #                                        ...PRESENTING...
         #                          A R T I F i C I A L  -  I N T E L i G E N C E
         #                                      .in  Auto-Filter-Bot.
-        #                                         @LazyDeveloperr
+        #                                         
 
                                       
 
@@ -98,77 +61,4 @@ async def lazy_answer(client, message):
 
 
 
-        # this line is for copy-pasters...
-        # ...while you are removing my credit and calling yourself a developerr... 
-        # _____ just imagine, At that time i am fucking your mom and sis at same time, harder & too harder...
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#
-#         i am NOT INTRESTED IN BEING YOUR SECOND FATHER... SO DON'T REMOVE MY CREDIT...
-#                                       @LazyDeveloperr
-# 
-# 
-# 
-# 
-#
-#
-#
-#
-#
-#
-#
-#                                                ...PRESENTING...
-#                                  A R T I F i C I A L  -  I N T E L i G E N C E
-#                                              .in  Auto-Filter-Bot.
-#                                                 @LazyDeveloperr
-#
-#                                       
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#         this line is for copy-pasters...
-#         ...while you are removing my credit and calling yourself a developerr... 
-#         _____ just imagine, At that time i am fucking your mom and sis at same time, harder & too harder...
-#
+        
